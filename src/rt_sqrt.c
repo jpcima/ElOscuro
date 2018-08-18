dc7db3b84cf8050e22810814035a3199057e049a
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2018-08-18 18:16:37 +0200

    Complete rewrite of rt_sqrt. Now using standard sqrt() function from math.h.

--- src/rt_sqrt.c
+++ src/rt_sqrt.c
@@ -1,82 +1,29 @@
-#include "rt_def.h"
+/*
+Copyright (C) 1994-1995 Apogee Software, Ltd.
+Copyright (C) 2002-2015 icculus.org, GNU/Linux port
+Copyright (C) 2018 Marc-Alexandre Espiaut
 
-#include "rt_sqrt.h"
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-/* 
-  C version of fixed-point Square Root functions 
- */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-long FixedSqrtLP(long n)  // Low  Precision (8.8)
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+#include <math.h>
+#include <stdint.h>
+
+int32_t
+FixedSqrtHP (int32_t n)
 {
-	/* 
-	   Not used, so unimplemented.
-	   If it matters, just copy the first half of the HP version,
-	   and multiply the answer by 256.
-	 */
-	STUB_FUNCTION;
-	
-	return 0;
-}
-
-long FixedSqrtHP(long n)  // High Precision (8.16)
-{
-	/* This is more or less a direct C transliteration of the asm code.
-	   I've replaced right shifting with division, since right shifting
-	   signed values is undefined in ANSI C (though it usually works).
-	   ROTT does not use this routine heavily. */
-	unsigned long root, mask, val;
-	signed long d;
-
-	root = 0;
-	mask = 0x40000000;
-	val = (unsigned long)n;
- hp1:
-	d = val;
-	d -= mask;
-	if (d < 0)
-	    goto hp2;
-	d -= root;
-	if (d < 0)
-	    goto hp2;
-	
-	val = d;
-	root /= 2;
-	root |= mask;
-	mask /= 4;
-	if (mask != 0)
-	    goto hp1;
-	else
-	    goto hp5;
- hp2:
-	root /= 2;
-	mask /= 4;
-	if (mask != 0)
-	    goto hp1;
-
- hp5:
-	mask = 0x00004000;
-	root <<= 16;
-	val <<= 16;
- hp3:
-	/* use add here to properly emulate the asm - SBF */
-	if ((root+mask) > val)
-	    goto hp4;
-	
-	val -= (root+mask);
-	   
-	root /= 2;
-	root |= mask;
-	mask /= 4;
-	if (mask != 0)
-	    goto hp3;
-	else
-	    goto hp6;
- hp4:
-	root /= 2;
-	mask /= 4;
-	if (mask != 0)
-	    goto hp3;
- hp6:
-
-	return (long)root;
+  return (int32_t)(sqrt((double) n) * 256);
 }
