fc2b35f3c74c6c52c189fae85ee5a7821f849530
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2018-08-19 17:20:29 +0200

    Revewing rt_rand.c. Removing private include.

--- src/rt_rand.h
+++ src/rt_rand.h
@@ -25,23 +25,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "develop.h"
 
-void InitializeRNG (void);
-int32_t GetRandomSeed (void);
-
-#if RANDOMTEST
-int32_t GameRNG (int8_t*, int32_t);
-#define GameRandomNumber(string, val) GameRNG(string, val)
-
-int32_t RNG (int8_t*, int32_t);
-#define RandomNumber(string, val) RNG(string, val)
-#else
-int32_t GameRNG (void);
 #define GameRandomNumber(string, val) GameRNG()
-
-int32_t RNG (void);
 #define RandomNumber(string, val) RNG()
-#endif
-
-void SetRNGindex (int32_t);
+int32_t GameRNG (void);
 int32_t GetRNGindex (void);
-
+int32_t GetRandomSeed (void);
+void InitializeRNG (void);
+int32_t RNG (void);
+void SetRNGindex (int32_t);
