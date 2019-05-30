eb587c6d3a4e9f04b0cd8f969f46bf8f7fcbd819
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2019-05-30 19:45:37 +0200

    Reviewing rt_scale.h.

--- src/rt_scale.h
+++ src/rt_scale.h
@@ -1,5 +1,7 @@
 /*
 Copyright (C) 1994-1995 Apogee Software, Ltd.
+Copyright (C) 2002-2015 icculus.org, GNU/Linux port
+Copyright (C) 2018-2019 Marc-Alexandre Espiaut
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -17,38 +19,31 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-//***************************************************************************
-//
-//    RT_SCALE.H - Scale-o-rama
-//
-//***************************************************************************
-#ifndef _rt_scale_public
-#define _rt_scale_public
+#pragma once
+
+#include <stdint.h>
 
 #include "rt_draw.h"
 
-extern  int dc_texturemid;
-extern  int dc_iscale;
-extern  int dc_invscale;
-extern  int centeryclipped;
-extern  int sprtopoffset;
-extern  int dc_yl;
-extern  int dc_yh;
-extern  byte * dc_source;
-extern  int transparentlevel;
+extern int32_t dc_invscale;
+extern int32_t dc_iscale;
+extern int32_t dc_texturemid;
+extern int32_t dc_yh;
+extern int32_t dc_yl;
+extern int32_t sprtopoffset;
+extern int32_t transparentlevel;
+extern uint8_t* dc_source;
 
-void ScaleShape (visobj_t * vis);
-void ScaleWeapon (int xcent, int yoffset, int shapenum);
-void DrawScreenSprite (int x, int y, int shapenum);
-void SetLightLevel (int height);
-void ScaleMaskedPost (byte * src, byte * buf);
-void DrawScreenSizedSprite (int lump);
-void ScaleTransparentPost (byte * src, byte * buf, int level);
-void ScaleTransparentShape (visobj_t * sprite);
-void ScaleSolidShape (visobj_t * sprite);
-void DrawUnScaledSprite (int x, int y, int shapenum, int shade);
-void DrawPositionedScaledSprite (int x, int y, int shapenum, int height, int type);
-void DrawNormalSprite (int x, int y, int shapenum);
-void R_DrawWallColumn (byte*);
-
-#endif
+void DrawNormalSprite (int32_t, int32_t, int32_t);
+void DrawPositionedScaledSprite (int32_t, int32_t, int32_t, int32_t, int32_t);
+void DrawScreenSizedSprite (int32_t);
+void DrawScreenSprite (int32_t, int32_t, int32_t);
+void DrawUnScaledSprite (int32_t, int32_t, int32_t, int32_t);
+void R_DrawWallColumn (uint8_t*);
+void ScaleMaskedPost (uint8_t*, uint8_t*);
+void ScaleShape (visobj_t*);
+void ScaleSolidShape (visobj_t*);
+void ScaleTransparentPost (uint8_t*, uint8_t*, int32_t);
+void ScaleTransparentShape (visobj_t*);
+void ScaleWeapon (int32_t, int32_t, int32_t);
+void SetLightLevel (int32_t);
